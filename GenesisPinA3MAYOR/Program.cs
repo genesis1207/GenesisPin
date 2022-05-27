c8bad15b93b454290228e14972fa6fe217fba35c
Usuario <Usuario@DESKTOP-EI85AUE>
2022-05-27 06:29:35 +0200

    Agregar archivos de proyecto.

---
+++ GenesisPinA3MAYOR/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenesisPinA3MAYOR
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int numr;
+            int numr2;
+            Console.WriteLine("ingrese primer numero");
+            numr = int.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese segundo numero");
+            numr2 = int.Parse(Console.ReadLine());
+            if (numr > numr2)
+            {
+                Console.WriteLine("el numero mayor es " + numr);
+            }
+            else if (numr = numr2)
+            {
+                Console.WriteLine("los numeros son iguales");
+            }
+            else
+            {
+                Console.WriteLine("el numero mayor es " + numr2);
+            }
+            Console.ReadKey();
+        }
+    }
+}
